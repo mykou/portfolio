6ec9f7bc56fef10b8caf24e55db5e22642db6292
Michael Liu <liu.michael.11@gmail.com>
2019-08-09 15:47:11 +1000

    set up layout and placeholder for about paragraphs

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,15 @@ import React from "react"
 import styled from "styled-components"
 
 const Layout = styled.div`
-	
+	& .content {
+		width: calc(100% - 20rem);	
+		@media only screen and (max-width: 768px) {
+			width: calc(100% - 10rem);	
+		}
+		@media only screen and (max-width: 480px) {
+			width: calc(100% - 5rem);	
+		}
+	}
 `
 
 export default ({ children }) => (  
--- src/components/portfolio/sections/about.jsx
+++ src/components/portfolio/sections/about.jsx
@@ -2,11 +2,40 @@ import React from 'react'
 import styled from "styled-components"
 
 const AboutContainer = styled.div`
-	min-height: 100vh;
+	min-height: 100vh;	
+	display: flex;
+	flex-flow: column;
+	align-items: center; 
+	justify-content: center; 
+`
+const Content = styled.div`		
+	height: 100%;
 `
 
 export default () => (
   <AboutContainer>
-  	
+  	<Content className="content">
+  		<h1>About</h1>
+  		<p>  			
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+				Sed dapibus lorem ac consectetur ultrices. 
+				Donec facilisis laoreet urna. 
+				Suspendisse condimentum ex sit amet elit suscipit, sed accumsan odio pharetra. 
+				Duis lacinia sagittis ipsum, non semper tortor cursus nec. 
+				Sed a quam cursus, euismod nunc a, volutpat augue. 
+				Nullam efficitur ut mi sit amet finibus. 
+				Duis pulvinar finibus condimentum.
+			</p>
+			<p>
+				Duis mattis nisi ac euismod eleifend. 
+				Donec viverra erat quis odio pulvinar, vitae convallis arcu dictum. 
+				Vivamus sit amet lectus sed sem facilisis elementum nec ut justo. 
+				Cras ornare dolor vel nulla consequat, hendrerit mollis arcu condimentum. 
+				Duis sollicitudin tellus maximus erat lobortis pellentesque. 
+				Fusce ac urna ac neque pharetra aliquet. 
+				Vestibulum dignissim sagittis erat at gravida. 
+				Etiam posuere congue vehicula.
+			</p>  		
+  	</Content>	
   </AboutContainer>
 )
